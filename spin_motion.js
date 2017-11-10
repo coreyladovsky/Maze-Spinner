d488a33717e29206d1525870028348ff0d28781b
Corey Ladovsky <coreyskymusic@gmail.com>
2017-11-09 22:53:06 -0500

    working rotation

--- spin_motion.js
+++ spin_motion.js
@@ -1,19 +1,53 @@
-  let looper;
-  let degrees = 0;
-function spinner(el,speed){
-	var item = document.getElementById(el);
-		item.style.transform = `rotate(${degrees}deg)`;
-  looper = setTimeout(() => {
-    spinner("canvas", 30)
-  }, 30);
-	degrees++;
-	if(degrees > 359){
-		degrees = 1;
-	}
-	// document.getElementById("status").innerHTML = "rotate("+degrees+"deg)";
+//   let looper;
+//   let degrees = 0;
+// function spinner(el,speed){
+// 	var item = document.getElementById(el);
+// 		item.style.transform = `rotate(${degrees}deg)`;
+//   looper = setTimeout(() => {
+//     spinner("canvas", 30)
+//   }, 30);
+// 	degrees++;
+// 	if(degrees > 359){
+// 		degrees = 1;
+// 	}
+// 	// document.getElementById("status").innerHTML = "rotate("+degrees+"deg)";
+// }
+// spinner("canvas", 30)
+var degrees = 0 ;
+let counter = 0;
+let array = [-2, -1, 1, 2];
+let angle;
+
+function rotate(angle){
+
+  degrees += angle
+  setTimeout(() => {
+    counter++ ;
+    let item = document.getElementById("canvas");
+    item.style.transform = `rotate(${degrees}deg)`;
+    if(counter < 90) {
+      setTimeout(() => {
+        rotate(angle);
+      }, 10);
+
+
+    }
+  }, 10);
 }
-spinner("canvas", 30)
 
-let counter = 0
+function randTime() {
+  return Math.round(Math.random() * (10000 - 5000) + (5000));
+}
 
-Math.round(Math.random() * 3)
+
+function keepSpinning() {
+  setTimeout(()=> {
+    counter = 0
+    angle = array[Math.round(Math.random() * 3)]
+    rotate(angle);
+    keepSpinning();
+  }, randTime());
+
+}
+
+// keepSpinning();
