807a39bc824f254d8e116cd35c65e497d00f6cfc
Corey Ladovsky <coreyskymusic@gmail.com>
2017-12-08 15:32:08 -0500

    bunldes

---
+++ bundle.js
@@ -0,0 +1,113 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports) {
+
+function spaceBar(e) {
+  if(e.keyCode === 32) {
+    $("#opening-screen").hide();
+    $("#you-win-screen").hide();
+    $("#gameOverScreen").hide();
+    $("#level2").hide();
+    $("#canvas").hide();
+    $("#level3").hide();
+    $("#level1").show();
+
+    startGame(30);
+    keepSpinning([-2, -1, 1, 2], 0, document.getElementById("level1"));
+  }
+}
+
+
+
+$("#gameOverScreen").hide();
+  $("#you-win-screen").hide();
+
+
+let looper;
+let deg = 0;
+function loops(el,speed){
+var item = document.getElementById(el);
+  item.style.transform = `rotate(${deg}deg)`;
+looper = setTimeout(() => {
+  loops(el, 50)
+}, 5);
+deg++;
+if(deg > 359){
+  deg = 1;
+}
+}
+
+opener([1,-1], 0, "spinner-text")
+
+
+document.addEventListener("keypress", spaceBar, {once: true})
+
+
+/***/ })
+/******/ ]);
+//# sourceMappingURL=bundle.js.map
